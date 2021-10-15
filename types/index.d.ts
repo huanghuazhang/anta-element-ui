fa838d96591a1f147ea3d344093d231e23ee2cb1
youpinyao <497400448@qq.com>
2021-10-15 23:53:41 +0800

    feat: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,8 @@
 export * from '../node_modules/element-ui/types/element-ui';
 import * as AntaElementUI from '../node_modules/element-ui/types/element-ui';
 import { AtButton } from './AtButton';
+import { AtIcon } from './AtIcon';
+import { AtClipboard } from './AtClipboard';
 
-export { AtButton };
+export { AtButton, AtIcon, AtClipboard };
 export default AntaElementUI;
