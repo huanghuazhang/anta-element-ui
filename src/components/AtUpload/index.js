621e68e6b2fe257a1308c7dd2e7bd103e573d0a2
youpinyao <497400448@qq.com>
2021-11-15 16:44:39 +0800

    feat: upload

--- src/components/AtUpload/index.js
+++ src/components/AtUpload/index.js
@@ -1,17 +1,27 @@
 import ElUpload from 'element-ui/packages/upload/src/index';
 import withStaticClass from '../../utils/withStaticClass';
+import UploadList from './UploadList.vue';
 
 const AtUpload = {
 	...ElUpload,
 	name: 'AtUpload',
+	components: {
+		...ElUpload.components,
+		UploadList
+	},
 	props: {
 		...ElUpload.props,
 		block: {
 			type: Boolean,
 			default: false
+		},
+		sizeLimit: {
+			type: Number,
+			default: 0
 		}
 	},
 	render(...args) {
+		const that = this;
 		const element = withStaticClass(
 			this,
 			{
@@ -19,7 +29,7 @@ const AtUpload = {
 				render() {
 					let uploadList;
 
-					if (this.showFileList) {
+					if (this.showFileList && this.uploadFiles.length) {
 						uploadList = (
 							<UploadList
 								disabled={this.uploadDisabled}
@@ -39,13 +49,32 @@ const AtUpload = {
 						);
 					}
 
+					const sizeText =
+						this.sizeLimit > 1024
+							? parseFloat((this.sizeLimit / 1024).toFixed(2)) + 'M'
+							: this.sizeLimit + 'K';
+
 					const uploadData = {
 						props: {
 							type: this.type,
 							drag: this.drag,
 							action: this.action,
 							multiple: this.multiple,
-							'before-upload': this.beforeUpload,
+							beforeUpload(...args) {
+								const [file] = args;
+
+								if (
+									file &&
+									that.sizeLimit &&
+									that.sizeLimit < file.size / 1024
+								) {
+									this.$message.error({
+										message: `单个文件不超过 ${sizeText}`
+									});
+									return false;
+								}
+								return that?.beforeUpload?.call(this, ...args);
+							},
 							'with-credentials': this.withCredentials,
 							headers: this.headers,
 							name: this.name,
@@ -67,8 +96,40 @@ const AtUpload = {
 						},
 						ref: 'upload-inner'
 					};
+					const tip = this.sizeLimit ? (
+						<div class="el-upload__tip">单个文件不超过&nbsp;{sizeText}</div>
+					) : (
+						undefined
+					);
+					const fileList = this.fileList || [];
 
-					const trigger = this.$slots.trigger || this.$slots.default;
+					let button = (
+						<at-button
+							disabled={fileList >= this.limit}
+							class="at-upload__button"
+							type="text"
+						>
+							<at-icon name="upload" class="at-icon--left"></at-icon>
+							<span>添加文件</span>
+							{this.limit ? (
+								<b>
+									{fileList.length}/{this.limit}
+								</b>
+							) : (
+								undefined
+							)}
+						</at-button>
+					);
+
+					if (this.listType === 'picture-card') {
+						button = (
+							<div class="at-upload__plus">
+								<at-icon name="plus"></at-icon>
+							</div>
+						);
+					}
+
+					const trigger = this.$slots.trigger || this.$slots.default || button;
 					const uploadComponent = <upload {...uploadData}>{trigger}</upload>;
 
 					return (
@@ -79,7 +140,7 @@ const AtUpload = {
 							{this.$slots.trigger
 								? [uploadComponent, this.$slots.default]
 								: uploadComponent}
-							{this.$slots.tip}
+							{this.$slots.tip || tip}
 						</div>
 					);
 				}
--- types/AtUpload.d.ts
+++ types/AtUpload.d.ts
@@ -1,3 +1,10 @@
 import { ElUpload } from 'element-ui/types/upload';
 /** Button Component */
-export declare class AtUpload extends ElUpload {}
+export declare class AtUpload extends ElUpload {
+	/** 是否display:block */
+	block: boolean;
+ /**
+	* 限制文件大小 单位 KB
+  */
+	sizeLimit: number;
+}
